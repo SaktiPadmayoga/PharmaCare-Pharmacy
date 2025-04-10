96725b8987e4e10a350f237efd64694737cac982
SaktiPadmayoga <sakti14yoga@gmail.com>
2025-04-10 16:13:16 +0800

    Updating addresses & medicines migration

--- database/migrations/2025_04_10_000006_create_medicines_table.php
+++ database/migrations/2025_04_10_000006_create_medicines_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('description');
             $table->decimal('price', 10, 2);
-            $table->string('stock');
+            $table->integer('stock')->default(0)->change();
             $table->string('image');
             $table->timestamps();
         });
