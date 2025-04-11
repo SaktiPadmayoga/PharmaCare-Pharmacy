3ddcb9f4f76edbc9ee51af17a54b45b93e13d6f8
SaktiPadmayoga <sakti14yoga@gmail.com>
2025-04-11 22:24:45 +0800

    Creating User Route

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,5 @@ Route::get('/', function () {
 Route::post('/users', [UserController::class, 'store'])->name('users.store');
 Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
 Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+Route::get('adminDashboard/customers', [UserController::class, 'showCustomers'])->name('admin.customers');
+Route::get('adminDashboard/staff', [UserController::class, 'showStafs'])->name('admin.staffs'); 
