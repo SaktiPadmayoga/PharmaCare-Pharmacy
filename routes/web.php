7a5767a3ea79ed72d37ae15679f0b4066d68f80c
SaktiPadmayoga <sakti14yoga@gmail.com>
2025-04-13 17:19:07 +0800

    Creating HomePage Route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MedicineController;
 use App\Http\Controllers\ProcurementController;
 use App\Http\Controllers\SupplierController;
@@ -10,9 +11,11 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+
+
+Route::get('Pharmacare', [HomeController::class, 'getRandomMedicine'])->name('homepage');
 
 
 Route::post('/users', [UserController::class, 'store'])->name('users.store');
